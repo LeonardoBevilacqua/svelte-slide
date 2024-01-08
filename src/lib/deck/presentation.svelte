96d272ef22d77adf30ab667d9936a79341394064
Leonardo Almeida Bevilacqua <lbevilacqua@lenovo.com>
2024-01-08 10:56:41 -0300

    creating markdown component

--- src/lib/deck/presentation.svelte
+++ src/lib/deck/presentation.svelte
@@ -1,39 +1,18 @@
 <script lang="ts">
-	import Code from './code.svelte';
-	import Slide from './slide.svelte';
+	// import Code from './code.svelte';
+	import Markdown from './markdown.svelte';
+	// import Slide from './slide.svelte';
 </script>
 
-<Slide animate>
-	<Code lines id="code">
-		{`
-        function love() {}
-        `}
-	</Code>
-</Slide>
-<Slide animate>
-	<Code lines id="code">
-		{`
-        function love() {
-            console.log('Svelte')
-        }
-        `}
-	</Code>
-</Slide>
+<Markdown>
+	{`
+        \`\`\`js [1-2|3|4]
+        let a = 1;
+        let b = 2;
+        let c = x => 1 + 2 + x;
+        c(3);
+        \`\`\`
+    `}
+</Markdown>
 
-<Slide animate>
-	<p class="uppercase">Transitions</p>
-	<div class="flex place-content-center gap-2">
-		<div data-id="box-1" class="h-[100px] w-[100px] bg-gray-400"></div>
-		<div data-id="box-2" class="h-[100px] w-[100px] bg-gray-400"></div>
-		<div data-id="box-3" class="h-[100px] w-[100px] bg-gray-400"></div>
-	</div>
-</Slide>
-
-<Slide animate>
-	<p class="uppercase">Transitions</p>
-	<div class="flex place-content-center gap-2">
-		<div data-id="box-1" class="h-[200px] w-[200px] bg-red-400"></div>
-		<div data-id="box-2" class="h-[200px] w-[200px] bg-green-400"></div>
-		<div data-id="box-3" class="h-[200px] w-[200px] bg-blue-400"></div>
-	</div>
-</Slide>
+<Markdown name="exemple.md" external />
