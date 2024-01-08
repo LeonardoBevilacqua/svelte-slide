e76a21d05ec8b7a0d164b83242d3253d2cf15f57
Leonardo Almeida Bevilacqua <lbevilacqua@lenovo.com>
2024-01-08 11:02:00 -0300

    creating notes component

--- src/lib/deck/presentation.svelte
+++ src/lib/deck/presentation.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
-	// import Code from './code.svelte';
-	import Markdown from './markdown.svelte';
-	// import Slide from './slide.svelte';
+	import Notes from './notes.svelte';
+	import Slide from './slide.svelte';
 </script>
 
-<Markdown>
-	{`
-        \`\`\`js [1-2|3|4]
-        let a = 1;
-        let b = 2;
-        let c = x => 1 + 2 + x;
-        c(3);
-        \`\`\`
-    `}
-</Markdown>
+<Slide>
+	<h1>Slide 1</h1>
 
-<Markdown name="exemple.md" external />
+	<Notes>Notes 1</Notes>
+</Slide>
+<Slide>
+	<h1>Slide 2</h1>
+
+	<Notes>Notes 2</Notes>
+</Slide>
